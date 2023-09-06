41825f92090c449e347eb392486f975038f09b2a
Kishlay-notabot <starlordcosmicfantasy@gmail.com>
2023-09-06 16:46:27 +0530

    working test 1

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,10 +1,34 @@
 import './App.css';
 
+
+
+
+
+const Testcomp = () => {
+  return (
+    <div>
+        <h1>Kishlay</h1>
+        <h2>@mail.com</h2>
+
+    </div>
+
+
+
+
+  );
+ 
+};
+
 function App() {
   return (
     <div className="App">
+     Hello World!
+      <Testcomp />
+
+
     </div>
   );
-}
+};
+
 
 export default App;
