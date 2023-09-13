557c139041e93dd0f0aac75c8c10eaa67cea417d
Kishlay-notabot <starlordcosmicfantasy@gmail.com>
2023-09-13 20:30:15 +0530

    started crud dev

--- crud_app/src/App.js
+++ crud_app/src/App.js
@@ -1,12 +1,13 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [crudTodo, todoFunc] = useState([]);
   return (
     <div className="App">
       <div className='inputs'><input /> 
       <button>enter</button>
       </div>
-
       <div className='list'></div>
      </div>
   );
