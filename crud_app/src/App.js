807bebb7432d42199a6bfe2c1e5a82cb52039144
Kishlay-notabot <starlordcosmicfantasy@gmail.com>
2023-09-13 20:48:56 +0530

    crud basic show input

--- crud_app/src/App.js
+++ crud_app/src/App.js
@@ -3,12 +3,19 @@ import './App.css';
 
 function App() {
   const [crudTodo, todoFunc] = useState([]);
+  const[newT, setT] = useState("");
+  const inpchg = (event)=>{
+    setT(event.target.value);
+  
+  }
   return (
     <div className="App">
-      <div className='inputs'><input /> 
+      <div className='inputs'><input onChange={inpchg} /> 
       <button>enter</button>
       </div>
-      <div className='list'></div>
+      <div className='list'>
+        {/* {newT} to show task realtime while typing */}
+      </div>
      </div>
   );
 }
