da01b341fc32ea44d8707872bfb7750160a86833
Kishlay-notabot <starlordcosmicfantasy@gmail.com>
2023-10-05 22:32:14 +0530

    2:46:40

--- apifl/src/App.js
+++ apifl/src/App.js
@@ -5,8 +5,15 @@ import { useEffect, useState } from 'react';
 
 
 function App() {
+  const [name,setName] = useState("")
+  const fetchD= () => {
+    Axios.get("https://api.agify.io/?name=")
+
+  }
   <div className='App'>
     <button>Predict_age</button>
+    <input placeholder='write ur name' onChange={(event) =>{setName(event.target.value)}}/>
+    <h1> Predicted_age= </h1>
   </div>
 }
 
