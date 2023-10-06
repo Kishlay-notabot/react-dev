9f09d8dfa45d2d5bac83115c2187e7c391241cc9
Kishlay-notabot <starlordcosmicfantasy@gmail.com>
2023-10-06 21:08:31 +0530

    basic code complete with console output only, not rendered

--- apifl/src/App.js
+++ apifl/src/App.js
@@ -1,20 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
 import Axios from "axios"; 
 import { useEffect, useState } from 'react';
 
 
 function App() {
+  console.log('rendered')
   const [name,setName] = useState("")
   const fetchD= () => {
     Axios.get(`https://api.agify.io/?name=${name}`).then((res) => {console.log(res.data)});
-
+// 2:47:39
   }
+  return(
   <div className='App'>
-    <button>Predict_age</button>
     <input placeholder='write ur name' onChange={(event) =>{setName(event.target.value)}}/>
+    <button onClick={fetchD}>Predict_age</button>
     <h1> Predicted_age= </h1>
   </div>
+  )
 }
 
 export default App;
