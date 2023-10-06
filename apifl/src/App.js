55da494992e3b95bc0b4d22e4c408b722bdfafa9
Kishlay-notabot <starlordcosmicfantasy@gmail.com>
2023-10-06 21:20:11 +0530

    fetch multiple data from object like name etc.

--- apifl/src/App.js
+++ apifl/src/App.js
@@ -5,18 +5,21 @@ import { useEffect, useState } from 'react';
 
 function App() {
   console.log('rendered')
-  const [predAge, setpredAge] = useState(0);
+  const [predAge, setpredAge] = useState(null);
   const [name,setName] = useState("")
   const fetchD= () => {
     Axios.get(`https://api.agify.io/?name=${name}`).then((res) => 
-    {setpredAge(res.data.age)});
+    {setpredAge(res.data)});
 // 2:47:39
   }
   return(
   <div className='App'>
     <input placeholder='write ur name' onChange={(event) =>{setName(event.target.value)}}/>
     <button onClick={fetchD}>Predict_age</button>
-    <h1> Predicted_age= {predAge}</h1>
+    <h1>name= {predAge?.name}</h1>
+    <h1> Predicted_age= {predAge?.age}</h1>
+    <h1> count= {predAge?.count}</h1>
+
   </div>
   )
 }
