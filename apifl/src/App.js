55937a153ae55dcaf15219b63035f7e7cfd8ee4e
Kishlay-notabot <starlordcosmicfantasy@gmail.com>
2023-10-06 21:10:46 +0530

    minor change in line 12, working code

--- apifl/src/App.js
+++ apifl/src/App.js
@@ -5,16 +5,18 @@ import { useEffect, useState } from 'react';
 
 function App() {
   console.log('rendered')
+  const [predAge, setpredAge] = useState(0);
   const [name,setName] = useState("")
   const fetchD= () => {
-    Axios.get(`https://api.agify.io/?name=${name}`).then((res) => {console.log(res.data)});
+    Axios.get(`https://api.agify.io/?name=${name}`).then((res) => 
+    {setpredAge(res.data.age)});
 // 2:47:39
   }
   return(
   <div className='App'>
     <input placeholder='write ur name' onChange={(event) =>{setName(event.target.value)}}/>
     <button onClick={fetchD}>Predict_age</button>
-    <h1> Predicted_age= </h1>
+    <h1> Predicted_age= {predAge}</h1>
   </div>
   )
 }
