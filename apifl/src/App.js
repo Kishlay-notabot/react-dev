b58c33198aa22f63b155f48ad4cc890ff3ec1784
Kishlay-notabot <starlordcosmicfantasy@gmail.com>
2023-10-22 10:16:26 +0530

    react router dom

--- apifl/src/App.js
+++ apifl/src/App.js
@@ -1,8 +1,13 @@
+import { Home } from './pages/Home';
 import './App.css';
-import {} from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 function App() {
   return(
   <div className='App'> 
+
+  <Router>
+    <Routes> <Route path='/' element={<Home />}/> </Routes>
+  </Router>
   </div>
   )
 }
