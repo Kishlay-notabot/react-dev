49b550faeae8eaa4a3ef9413b16047a4193c86a7
Kishlay-notabot <starlordcosmicfantasy@gmail.com>
2023-11-09 17:28:34 +0530

    footnote

--- apifl/src/pages/contact.js
+++ apifl/src/pages/contact.js
@@ -1,17 +1,13 @@
-import { useState } from "react";
-
 export const Profile = (props)=> {
-    const [newUsername, SetNU] = useState("")
     return<h1> Profile = {props.username}
-    <ChangeProfile SetNU={props.setUsern}/>
+    <ChangeProfile />
     </h1> 
 };
-export const ChangeProfile = (props) => {
+export const ChangeProfile = () => {
 return <div>
-    <input onChange={(event)=>{setUsern(event.target.value)
-    }}
-    />
-    <button onClick={()=>{props.setUsern(newUsername)}}>Change username</button>
+    <input />
+    <button>Change username</button>
 </div>
 }
 
+// 3:16:05 context api
