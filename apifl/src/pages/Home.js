45856502d7d85fcdd6b5b73cdf971794fa3db0cb
Kishlay-notabot <starlordcosmicfantasy@gmail.com>
2023-11-25 13:39:08 +0530

    error not solved after rewriting

--- apifl/src/pages/Home.js
+++ apifl/src/pages/Home.js
@@ -1,18 +1,14 @@
 import { useQuery } from '@tanstack/react-query'
-import { useContext } from "react";
-import { AppContext } from '../App'
 import Axios from "axios";
-export const Home = (props)=> {
 
-    const {data} = useQuery(["cat"], ()=>{ return Axios.get("https://catfact.ninja/fact").then((res) => res.data);
+export const Home = () => {
+    const { data } = useQuery(["cat"], () => {
+        return Axios.get("https://catfact.ninja/fact").then((res) => res.data);
 
-
-        } );
-
-
-
-    const { username } = useContext(AppContext);
-    return<h1>Home page working <p>{data.fact}</p>
-        welcome {username}
-    </h1>
+    });
+    return (
+        <h1>
+            This is the home page <p>{data?.fact}</p>
+        </h1>
+    );
 };
