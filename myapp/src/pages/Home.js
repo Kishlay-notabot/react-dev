8c00021e7cefa4ef7b42f729cb5bcf9bbb764513
Kishlay-notabot <starlordcosmicfantasy@gmail.com>
2023-11-25 15:28:41 +0530

    old code not working, something not working properly

--- myapp/src/pages/Home.js
+++ myapp/src/pages/Home.js
@@ -1,14 +1,8 @@
-import { useQuery } from '@tanstack/react-query'
-import Axios from "axios";
-
-export const Home = () => {
-    const { data } = useQuery(["cat"], () => {
-        return Axios.get("https://catfact.ninja/fact").then((res) => res.data);
-
-    });
-    return (
-        <h1>
-            This is the home page <p>{data?.fact}</p>
-        </h1>
-    );
+import { useContext } from "react";
+import { AppContext } from '../App'
+export const Home = ()=> {
+    const { username } = useContext(AppContext);
+    return<h1>Home page working
+        welcome {username}
+    </h1>
 };
