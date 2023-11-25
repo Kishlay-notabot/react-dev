4dfe4015ac0018b026b1ff1eff3429bd446f7e1c
Kishlay-notabot <starlordcosmicfantasy@gmail.com>
2023-11-25 13:24:34 +0530

    edited home.js, error caught

--- apifl/src/pages/Home.js
+++ apifl/src/pages/Home.js
@@ -1,14 +1,18 @@
 import { useQuery } from '@tanstack/react-query'
 import { useContext } from "react";
 import { AppContext } from '../App'
+import Axios from "axios";
 export const Home = (props)=> {
 
-    const {} = useQuery(["cat"], );
+    const {data} = useQuery(["cat"], ()=>{ return Axios.get("https://catfact.ninja/fact").then((res) => res.data);
+
+
+        } );
 
 
 
     const { username } = useContext(AppContext);
-    return<h1>Home page working
+    return<h1>Home page working <p>{data.fact}</p>
         welcome {username}
     </h1>
 };
