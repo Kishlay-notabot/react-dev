031f5b96dd964e4c7ad5e23aa8315521ea87742e
Kishlay-notabot <starlordcosmicfantasy@gmail.com>
2023-11-26 22:41:14 +0530

    error trial 3

---
+++ jsproj/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import Axios from 'axios'
+import { useQuery } from "@tanstack/react-query";
+function App() {
+  const { data } = useQuery(["cat"], () => {
+    return Axios.get("https://catfact.ninja/facts").then((res) => res.data);
+  })
+  return (
+
+    <div className="App">
+      hi
+      <p>{data?.fact}</p>
+
+    </div>
+  );
+}
+
+export default App;
