6380cff4569c9720213942358d44b0020376609e
Kishlay-notabot <starlordcosmicfantasy@gmail.com>
2023-11-26 22:00:56 +0530

    remove app

--- myapp/src/pages/Home.js
+++
@@ -1,8 +0,0 @@
-import { useContext } from "react";
-import { AppContext } from '../App'
-export const Home = ()=> {
-    const { username } = useContext(AppContext);
-    return<h1>Home page working
-        welcome {username}
-    </h1>
-};
