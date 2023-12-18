dac8c55af2942416450a87ddb922902b15a1d8c9
Kishlay-notabot <starlordcosmicfantasy@gmail.com>
2023-12-18 20:32:11 +0530

    try basic code

--- jsproj/src/App.js
+++ jsproj/src/App.js
@@ -2,14 +2,19 @@ import './App.css';
 import Axios from 'axios'
 import { useQuery } from "@tanstack/react-query";
 function App() {
-  const { data } = useQuery(["cat"], () => {
-    return Axios.get("https://catfact.ninja/facts").then((res) => res.data);
-  })
+  // const { data } = useQuery(["cat"], () => {
+  //   return Axios.get("https://catfact.ninja/facts").then((res) => res.data);
+  // })
   return (
 
     <div className="App">
       hi
-      <p>{data?.fact}</p>
+      {/* <p>{data?.fact}</p> */} 
+
+    <p>sdfs
+      lol</p>
+      
+
 
     </div>
   );
