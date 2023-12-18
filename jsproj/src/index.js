02d006b6ef530f70dfbcd900d7f1ba1be0670126
Kishlay-notabot <starlordcosmicfantasy@gmail.com>
2023-12-18 20:54:05 +0530

    formatted code

--- jsproj/src/index.js
+++ jsproj/src/index.js
@@ -1,17 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import { Home } from './App';
+import { Home } from './App';  // Import Home directly, not as default
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  
-    <Home />
-
+  <Home />
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
