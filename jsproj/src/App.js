edc30e7f95da8df33682603677f655c0dd52d6e0
Kishlay-notabot <starlordcosmicfantasy@gmail.com>
2023-12-20 00:47:02 +0530

    code trial

--- jsproj/src/App.js
+++ jsproj/src/App.js
@@ -1,16 +1,29 @@
-import { useQuery } from "@tanstack/react-query";
+import React from "react";
+import { useQuery, QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import Axios from "axios";
+const queryClient = new QueryClient();
 
 export const Home = () => {
   const { data } = useQuery({
-    queryKey: ["cat"],
+    queryKey: "cat",
     queryFn: () => {
       return Axios.get("https://catfact.ninja/fact").then((res) => res.data);
-  }})
-  return (
-    <h1>HOPE
-      <p>{data?.fact}</p>
+    },
+    //...
+  });
 
+  return (
+    <h1>
+      HOPE
+      <p>{data?.fact}</p>
     </h1>
   );
 };
+
+const App = () => (
+  <QueryClientProvider client={queryClient}>
+    <Home />
+  </QueryClientProvider>
+);
+
+export default App;
--- jsproj/src/index.js
+++ jsproj/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import { Home } from './App';  // Import Home directly, not as default
+import {Home} from './App';  // Import Home directly, not as default
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
