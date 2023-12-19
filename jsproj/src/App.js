4d4fd3cbea687c8ef0017ded52e5b1b52dbed297
Kishlay-notabot <starlordcosmicfantasy@gmail.com>
2023-12-19 18:29:22 +0530

    cleaned code.

--- jsproj/src/App.js
+++ jsproj/src/App.js
@@ -3,16 +3,14 @@ import { QueryClient, QueryClientProvider, useQuery } from 'react-query';
 const queryClient = new QueryClient();
 
 const CatFact = () => {
-  const { data, status } = useQuery('catFact', () =>
+  const { data } = useQuery('catFact', () =>
     fetch('https://catfact.ninja/fact').then((res) => res.json())
   );
 
   return (
     <div>
       <h2>Cat Fact</h2>
-      {status === 'loading' && <p>Loading...</p>}
-      {status === 'error' && <p>Error fetching cat fact</p>}
-      {status === 'success' && <p>{data.fact}</p>}
+      <p>{data?.fact}</p>
     </div>
   );
 };
