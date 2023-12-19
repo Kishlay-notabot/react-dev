b88dbbdea7afd655e7bbf9ef26d53fe9f8c50502
Kishlay-notabot <starlordcosmicfantasy@gmail.com>
2023-12-19 18:03:35 +0530

    Edit code

--- jsproj/src/App.js
+++ jsproj/src/App.js
@@ -3,9 +3,11 @@ import Axios from "axios"
 
 
 export const Home = () => {
-  const { data } = useQuery(["cat"],() => {
-    return Axios.get("https://catfact.ninja/fact").then((res) => res.data);
-  
+  const { data } = useQuery({
+    queryKey: ["cat"],
+    queryFn: () => {
+      return Axios.get("https://catfact.ninja/fact").then((res) => res.data);
+  }
   });
 
   return (
