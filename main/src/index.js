b644819e43972585e53cea46c251e85714f06225
Kishlay-notabot <starlordcosmicfantasy@gmail.com>
2024-01-08 23:51:17 +0530

    edit

--- main/src/index.js
+++ main/src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { QueryClientProvider } from "@tanstack/react-query";
-
+import {QueryClient, QueryClientProvider } from "@tanstack/react-query";
+const queryClient = new QueryClient();
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
