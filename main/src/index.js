9c322da3500ec4cb34d57609d3bffe97e914fd51
Kishlay-notabot <starlordcosmicfantasy@gmail.com>
2024-01-09 19:54:39 +0530

    change window focus behaviour

--- main/src/index.js
+++ main/src/index.js
@@ -4,7 +4,11 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {QueryClient, QueryClientProvider } from "@tanstack/react-query";
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({defaultOptions:{
+   queries: {
+    refetchOnWindowFocus: false,
+   }
+}});
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 
