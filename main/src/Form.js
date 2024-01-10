173daebeb7e7d06fddd770d68a08dc375cc0ef3e
Kishlay-notabot <starlordcosmicfantasy@gmail.com>
2024-01-10 20:56:29 +0530

    add data log in console

--- main/src/Form.js
+++ main/src/Form.js
@@ -1,15 +1,17 @@
 import { useForm } from 'react-hook-form';
-
+import * as yup from 'yup'
 export const Form = () => {
     const { register, handleSubmit } = useForm();
-    const onSubmit = () => {
+    const onSubmit = (data) => {
         console.log('Hello boyd');
+        console.log(data)
     }
     return(
         <form onSubmit={handleSubmit(onSubmit)}>
-            <input type="text" placeholder="Name" /> <br />
-            <input type="text" placeholder="Email" /> <br />
-            <input type="password" placeholder="Pass" /> <br />
+            <input type="text" placeholder="Name" {...register("fName")} /> <br />
+            <input type="text" placeholder="Email" {...register("email")}/> <br />
+            <input type="password" placeholder="Pass"{...register("passwd")} /> <br />
+            <input type="password" placeholder="confirmPass"{...register("rePasswd")} /> <br />
             <input type="submit"/>
         </form>
     )
