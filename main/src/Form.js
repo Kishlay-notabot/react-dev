2d99a121830d8ca78775fd7cbc295372758096f7
Kishlay-notabot <starlordcosmicfantasy@gmail.com>
2024-01-10 20:28:21 +0530

    init rhf

--- main/src/Form.js
+++ main/src/Form.js
@@ -1,6 +1,12 @@
+import { useForm } from 'react-hook-form';
+
 export const Form = () => {
+    const { register, handleSubmit } = useForm();
+    const onSubmit = () => {
+        console.log('Hello boyd');
+    }
     return(
-        <form>
+        <form onSubmit={handleSubmit(onSubmit)}>
             <input type="text" placeholder="Name" /> <br />
             <input type="text" placeholder="Email" /> <br />
             <input type="password" placeholder="Pass" /> <br />
