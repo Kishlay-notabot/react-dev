b243518dd283b51ed53ce175d0a10f5471ba8e74
Kishlay-notabot <starlordcosmicfantasy@gmail.com>
2024-01-12 12:59:56 +0530

    add form error message but does not work with passwd field

--- main/src/Form.js
+++ main/src/Form.js
@@ -4,11 +4,11 @@ import {yupResolver} from '@hookform/resolvers/yup'
 export const Form = () => {
     const schema = yup.object().shape(
         {
-            fName: yup.string().required(),
-            email: yup.string().email().required(),
-            passwd: yup.string().min(7).max(14).required(),
+            fName: yup.string().required("Enter Full Name first!"),
+            email: yup.string().email().required("enter correct email add!"),
+            passwd: yup.string().min(7).max(14).required("enter passwd between 7-14 char!"),
             // age: yup.number().positive().integer().min(18).required() [template for age]
-            confirmPass: yup.string().oneOf([yup.ref("passwd"), null]),
+            rePasswd: yup.string().oneOf([yup.ref("passwd"), null]).required("passwd should match!"),
 
         }
     )
@@ -24,10 +24,14 @@ export const Form = () => {
     return(
         <form onSubmit={handleSubmit(onSubmit)}>
             <input type="text" placeholder="Name" {...register("fName")} /> <br />
-            <p>{errors.fName?.message}4:14:49</p>
-            <input type="text" placeholder="Email" {...register("email")}/> <br />
-            <input type="password" placeholder="Pass"{...register("passwd")} /> <br />
-            <input type="password" placeholder="confirmPass"{...register("rePasswd")} /> <br />
+            <p>{errors.fName?.message}<br /></p>
+            <input type="text" placeholder="Email" {...register("email")}/>
+            <p>{errors.email?.message} <br /></p> 
+            
+            <input type="password" placeholder="Pass"{...register("passwd")} /> 
+            <p>{errors.passwd?.message}<br /></p>
+            <input type="password" placeholder="confirmPass"{...register("rePasswd")} /> 
+           <p>{errors.rePasswd?.message}<br /></p> 
             <input type="submit"/>
         </form>
     )
