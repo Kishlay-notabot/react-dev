7d635e9c4a3da3b57924df4b6932abaf26da9375
kishlay-notabot <kishlay.work1@gmail.com>
2024-05-19 15:12:04 +0530

    connected to mongodb

--- backend/server.js
+++ backend/server.js
@@ -3,5 +3,8 @@ const mongoose = require('mongoose')
 require('dotenv').config()
 const app = express()
 const PORT = process.env.port || 5000
-
+mongoose
+.connect(process.env.MONGODB_URL)
+.then(()=> console.log(`connected to mongodb`))
+.catch((err) => console.log(`error`))
 app.listen(PORT, () => console.log(`listening on ${PORT}`))
