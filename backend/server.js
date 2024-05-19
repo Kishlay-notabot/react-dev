3a6d829c8427e57b152a4b9185454c3914e6b967
kishlay-notabot <kishlay.work1@gmail.com>
2024-05-19 15:27:55 +0530

    Add routes

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const routes = require('./routes/todoroute')
 require('dotenv').config()
 const app = express()
 const PORT = process.env.port || 5000
 mongoose
-.connect(process.env.MONGODB_URL)
-.then(()=> console.log(`connected to mongodb`))
-.catch((err) => console.log(`error`))
+    .connect(process.env.MONGODB_URL)
+    .then(()=> console.log(`connected to mongodb`))
+    .catch((err) => console.log(`error`))
+    app.use(routes)
 app.listen(PORT, () => console.log(`listening on ${PORT}`))
